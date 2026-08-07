dabd82bac31af422edbaf6bb9d998dbf36f63c6c
Safiyo Mohamed <Smohamed166@student.cscc.edu>
2026-08-07 17:19:34 -0400

    Add docstring content

---
+++ Lab16_Safiyo-1.py
@@ -0,0 +1,8 @@
+"""
+Program Name: Lab16_Safiyo-1.py
+Author: Safiyo Mohamed
+Purpose: Reads Ohio's unemployment rate data and creates
+a time series line plot of the unemployment rate using matplotlib.
+Starter Code: No external starter code was used.
+Date: 08/09/2026
+"""

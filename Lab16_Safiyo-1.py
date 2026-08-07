6b33f7c1a00395839177eca2b5768bdb607affab
Safiyo Mohamed <Smohamed166@student.cscc.edu>
2026-08-07 17:21:30 -0400

    Add imports

--- Lab16_Safiyo-1.py
+++ Lab16_Safiyo-1.py
@@ -6,3 +6,6 @@ a time series line plot of the unemployment rate using matplotlib.
 Starter Code: No external starter code was used.
 Date: 08/09/2026
 """
+import csv
+import matplotlib.pyplot as plt
+from datetime import datetime 
